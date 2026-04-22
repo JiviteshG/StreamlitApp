583469aec9acbfaebb683c55f498fd67644ed4cd
Jivitesh Gudekar <gudekar.jivitesh@gmail.com>
2026-04-22 10:54:36 -0300

    simple chat bot with loren ipsum output

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import time
+
 import streamlit as st
 
 if 'messages' not in st.session_state:
     st.session_state['messages'] = []
+
+def generate_response():
+    response = """
+    loren ipsum dolor sit amet, consectetur adipiscing elit. 
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    """
+
+    for token in response.split(" "):
+        time.sleep(0.1)  # Simulate delay
+        yield token + " "
+
+    return f"Echo: {user_input}"
 
 for message in st.session_state.messages:
     if message['role'] == 'user':
@@ -16,6 +33,11 @@ if user_input and user_input != "":
     st.chat_message("user").markdown(user_input)
 
     # Simulate AI response (replace with actual AI logic)
-    ai_response = f"Echo: {user_input}"
-    st.session_state.messages.append({"role": "ai", "content": ai_response})
-    st.chat_message("ai").markdown(ai_response)
+    with st.chat_message("ai"):
+        response_generator = generate_response()
+        response = st.write_stream(response_generator)
+
+
+    st.session_state.messages.append({"role": "ai", "content": response})
+    
+    # st.chat_message("ai").markdown(ai_response)
